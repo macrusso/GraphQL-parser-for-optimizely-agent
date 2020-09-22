550b996a52e1bfbef3c8bacb5b5b88490417c089
macrusso <mac.rusek@pm.me>
2020-09-22 11:28:51 +0100

    init

---
+++ index.js
@@ -0,0 +1,48 @@
+const { ApolloServer, gql } = require('apollo-server');
+const AgentAPI = require('./rest')
+
+const typeDefs = gql`
+  type Query {
+    activate(userId: String!, experimentKey: String!): String
+    getVariation(userId: String!, experimentKey: String!): String
+    getForcedVariation(userId: String!, experimentKey: String!): String
+    getEnabledFeatures(userId: String!): [String]
+    isFeatureEnabled(userId: String!, featureKey: String!): Boolean
+    getConfig: String
+  }
+
+  type Mutation {
+    setForcedVariation(userId: String!, experimentKey: String!, variationKey:String): Boolean
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getConfig: (_, __, { dataSources }) =>
+      dataSources.agentAPI.getConfig(),
+    activate: (_, args, { dataSources }) =>
+      dataSources.agentAPI.activate({ ...args }),
+    getVariation: (_, args, { dataSources }) =>
+      dataSources.agentAPI.activate({ ...args }),
+    getEnabledFeatures: (_, args, { dataSources }) =>
+      dataSources.agentAPI.getEnabledFeatures({ ...args }),
+    isFeatureEnabled: (_, args, { dataSources }) =>
+      dataSources.agentAPI.isFeatureEnabled({ ...args }),
+  },
+  Mutation: {
+    setForcedVariation: (_, args, { dataSources }) =>
+      dataSources.agentAPI.setForcedVariation({ ...args }),
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    agentAPI: new AgentAPI(),
+  }),
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
---
+++ rest.js
@@ -0,0 +1,56 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class AgentAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'http://localhost:8080/v1/';
+  }
+
+  willSendRequest(request) {
+    // TODO: fetch it from request headers when live
+    request.headers.set('X-Optimizely-SDK-Key', 'HSvqwnCy51LdTmpAk54ntv');
+  }
+
+  async getConfig() {
+    const response = await this.get(`config`);
+    return response?.revision
+  }
+
+  async activate({ userId, experimentKey }) {
+    const response = await this.post(`activate?experimentKey=${experimentKey}`, {
+      userId
+    });
+    // TODO can there be more experiments activated with one key? If yes refactor response
+    return response[0].variationKey
+  }
+
+  async getEnabledFeatures({ userId }) {
+    const response = await this.post(`activate?type=feature`, {
+      userId
+    });
+    const enabledFeatures = response.reduce((acc, cur) => {
+      acc.push(cur.featureKey)
+      return acc
+    }, [])
+    return enabledFeatures
+  }
+
+  async isFeatureEnabled({ userId, featureKey }) {
+    const response = await this.post(`activate?featureKey=${featureKey}`, {
+      userId
+    });
+    // TODO can there be more feature activated with one key? If yes refactor response
+    return response[0].enabled
+  }
+
+  async setForcedVariation({ userId, experimentKey, variationKey }) {
+    const response = await this.post(`override`, {
+      userId,
+      experimentKey,
+      variationKey
+    });
+    return response?.variationKey === variationKey ? true : false
+  }
+}
+
+module.exports = AgentAPI;
